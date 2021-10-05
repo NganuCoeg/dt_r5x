401256ab76dc8f76f4977c84e25a4fe465004bbd
ItsVixano <vixanonew@gmail.com>
2021-10-05 07:46:41 +0530

    r5x: Build AOSP messaging app and Themepicker
    Change-Id: I3a25512bcb42aa19e4f6e17ccf204ee004577f2d
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -44,6 +44,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# ThemePicker
+PRODUCT_PACKAGES += \
+    ThemePicker \
+    messaging
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
